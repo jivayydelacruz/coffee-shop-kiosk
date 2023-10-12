8b99a91b0543a9fd52b8531dcacf49a710724d88
Lyzza Flores <89468418+liechee@users.noreply.github.com>
2023-10-12 17:22:28 +0800

    Fixed migration issues

--- coffiene/src/supabase.js
+++ coffiene/src/supabase.js
@@ -1,8 +1,8 @@
 const { SupabaseClient } = require("@supabase/supabase-js");
 
 const supabase = new SupabaseClient(
-  "https://ycqarkitulzvkawcpnbe.supabase.co", //process.env.SUPABASE_URL
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljcWFya2l0dWx6dmthd2NwbmJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0MjIxNzEsImV4cCI6MjAxMTk5ODE3MX0.UgfNedtCqV6wS4vId7GXNmFn7wIhX27f5EQyr5lIw98" //process.env.SUPABASE_ANON_KEY
+  "https://orbnotirwvdxfyasqpxe.supabase.co", //process.env.SUPABASE_URL
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9yYm5vdGlyd3ZkeGZ5YXNxcHhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0ODkxOTIsImV4cCI6MjAxMjA2NTE5Mn0.QNjgtLY7SrFWyD9d2z45PlLlKIcd8jD1ghpnjPHQk3I" //process.env.SUPABASE_ANON_KEY
 );
 
 module.exports = {
